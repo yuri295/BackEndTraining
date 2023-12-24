85c5b4e4a91bb4edd5d3b17c57546d64b825b270
yuri295 <jin_astro0302@naver.com>
2023-12-24 21:28:59 +0900

    DataType

---
+++ src/CharExam.java
@@ -0,0 +1,11 @@
+public class CharExam {
+    public static void main(String[] args) {
+        int a = '1';
+        int b = '2';
+        int c = '3';
+        int d = '4';
+        int e = '5';
+        int sum = ('1'+'2'+'3'+'4'+'5')-('0'*5);
+        System.out.println("sum = " + sum);
+    }
+}
---
+++ src/FindAPI.java
@@ -0,0 +1,11 @@
+public class FindAPI {
+    public static void main(String[] args) {
+        int a; //변수선언
+        a = 10;
+        System.out.println("a = " + a);
+        String s;
+        s = "APPLE";
+        System.out.println(s);
+        System.out.println(s.length());
+    }
+}
