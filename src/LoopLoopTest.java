bf0350a0c53065197661565d62a22947e8820c04
yuri295 <jin_astro0302@naver.com>
2023-12-28 17:14:44 +0900

    이중for문

---
+++ src/LoopLoopTest.java
@@ -0,0 +1,23 @@
+public class LoopLoopTest {
+    public static void main(String[] args) {
+        int index=1;
+        for(int i=0;i<5;i++){
+            for(int j=0;j<5;j++){
+                System.out.println(index);
+                index++;
+
+            }
+        }
+        int[][] a = {
+                {1,2,3,4,5},
+                {1,2,3,4,5}
+        };
+
+        for(int i=0;i<a.length;i++){
+            for(int j=0;j<a[i].length;j++){
+                System.out.println(a[i][j] + "\t");
+            }
+            System.out.println();
+        }
+    }
+}
