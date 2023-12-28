931cc0184fb89ce6b2add740d98bc511aa286a10
yuri295 <jin_astro0302@naver.com>
2023-12-28 12:07:23 +0900

    while문

---
+++ src/WhileLoopTest.java
@@ -0,0 +1,15 @@
+public class WhileLoopTest {
+    public static void main(String[] args) {
+        int i = 0;
+        while (i<=5){
+            System.out.println(i);
+            i++;
+        }
+
+        i=1;
+        do{
+            System.out.println(i);
+            i++;
+        }while (i<=5);
+    }
+}
